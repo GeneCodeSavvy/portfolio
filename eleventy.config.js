dfb9fc63da9a282c5a99e4d6b589074fb2add740
GeneCodeSavvy <hxrsh.09@gmail.com>
2025-06-07 12:10:25 +0530

    first commit template

---
+++ eleventy.config.js
@@ -0,0 +1,27 @@
+// import path from "node:path";
+import syntaxHighlight from "@11ty/eleventy-plugin-syntaxhighlight";
+import relativeLinks from "./_config/relative-links.js";
+
+export default function (eleventyConfig) {
+	eleventyConfig.addPassthroughCopy("./src/assets/favicon");
+	eleventyConfig.addPlugin(relativeLinks);
+    eleventyConfig.addPlugin(syntaxHighlight);
+	// eleventyConfig.addFilter("relativePath", function (targetPath, pageUrl) {
+	// 	const from = pageUrl.endsWith("/") ? pageUrl : path.dirname(pageUrl);
+	// 	return path.relative(from, targetPath).replace(/\\/g, "/");
+	// });
+
+	return {
+		dir: {
+			input: "src",
+			output: "_site",
+			includes: "_includes",
+			data: "_data", // For global data, e.g., site.json
+		},
+		// Use Nunjucks for templates, HTML for HTML files
+		templateFormats: ["njk", "md", "html"],
+		markdownTemplateEngine: "njk",
+		htmlTemplateEngine: "njk",
+		dataTemplateEngine: "njk",
+	};
+}
